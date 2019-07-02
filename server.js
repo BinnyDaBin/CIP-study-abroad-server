87b120d42f5b46a9f1acf539bccb56241ad4c595
Dabin Lee <dabinlee0918@gmail.com>
2019-07-02 17:49:45 -0400

    fix($access): CORS errors
    Fixed "No ‘Access-Control-Allow-Origin’ header is present on the requested resource" error by installing and using CORS library.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const app = express();
+
+// Then use it before your routes are set up:
+app.use(cors());
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to theCIP API' }));
 
