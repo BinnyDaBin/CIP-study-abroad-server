7d51a9206b3e04364ec32d4de95cdeea114f370d
Dabin Lee <dabinlee0918@gmail.com>
2019-07-17 01:43:19 -0400

    feat($courses): add mock data and filter data
    Mock Data contains 30 courses and can be filter by filters from user and paignator(offset, size).

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const MOCK_COURSES = require('./mock-courses.json');
+const _ = require('lodash');
 
+/*
+  @route  GET /courses
+  @desc   Get courses with filter and paignation
+  @access public
+*/
 router.get('/', (req, res) => {
-  res.send('Hello World');
+  const filters = req.query.filters;
+  const offset = parseInt(req.query.offset);
+  const size = parseInt(req.query.size);
+
+  const filtered = _.filter(MOCK_COURSES, JSON.parse(filters));
+  const start = offset * size;
+  const end = (offset + 1) * size;
+  const sliced = _.slice(filtered, start, end);
+  res.json(sliced);
 });
 
 module.exports = router;
