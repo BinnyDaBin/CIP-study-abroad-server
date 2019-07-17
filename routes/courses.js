c9d82d7245f0f9d76784e1f24cfac8f81bd485f6
Dabin Lee <dabinlee0918@gmail.com>
2019-07-17 11:20:43 -0400

    chore($courses): get request to have try and catch error

--- routes/courses.js
+++ routes/courses.js
@@ -9,15 +9,20 @@ const _ = require('lodash');
   @access public
 */
 router.get('/', (req, res) => {
-  const filters = req.query.filters;
-  const offset = parseInt(req.query.offset);
-  const size = parseInt(req.query.size);
+  try {
+    const filters = req.query.filters;
+    const offset = parseInt(req.query.offset);
+    const size = parseInt(req.query.size);
 
-  const filtered = _.filter(MOCK_COURSES, JSON.parse(filters));
-  const start = offset * size;
-  const end = (offset + 1) * size;
-  const sliced = _.slice(filtered, start, end);
-  res.json(sliced);
+    const filtered = _.filter(MOCK_COURSES, JSON.parse(filters));
+    const start = offset * size;
+    const end = (offset + 1) * size;
+    const sliced = _.slice(filtered, start, end);
+    res.json(sliced);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
