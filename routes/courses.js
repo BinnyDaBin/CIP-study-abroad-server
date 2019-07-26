8998e9a8b35f13b8c456f5fbf35a7cb1ac56b436
Dabin Lee <dabinlee0918@gmail.com>
2019-07-26 14:57:53 -0400

    chore($courses): fix suggested comments

--- routes/courses.js
+++ routes/courses.js
@@ -16,7 +16,6 @@ router.get('/', (req, res) => {
 
     const parsedFilters = JSON.parse(filters);
 
-    // Get each column filter and combine each to make final filtered
     const filterKeys = Object.keys(parsedFilters);
     const filtered = _.filter(MOCK_COURSES, course => {
       for (let i = 0; i < filterKeys.length; i++) {
@@ -24,55 +23,22 @@ router.get('/', (req, res) => {
         const filterValue = parsedFilters[filterKey];
         const courseValue = course[filterKey];
 
-        // if column filter value is empty then treat it as select all
-        if (_.isEmpty(filterValue)) continue;
-
-        // check if filter contains course value
-        if (!filterValue.includes(courseValue)) return false;
+        if (!filterValue.includes(courseValue)) {
+          return false;
+        }
       }
       return true;
     });
 
-    // select options for each column of filters
-    const allStudyAbroadInstitutions = _.map(
-      MOCK_COURSES,
-      'studyAbroadInstitution'
+    const columnIds = Object.keys(MOCK_COURSES[0]);
+    const columnOptions = _.transform(
+      columnIds,
+      (allOptions, columnId) => {
+        allOptions[columnId] = _.uniq(_.map(MOCK_COURSES, columnId));
+      },
+      {}
     );
-    const uniqueStudyAbroadInstitutions = _.uniq(allStudyAbroadInstitutions);
 
-    const allProgramNames = _.map(MOCK_COURSES, 'programName');
-    const uniqueProgramNames = _.uniq(allProgramNames);
-
-    const allYears = _.map(MOCK_COURSES, 'year');
-    const uniqueYears = _.uniq(allYears);
-
-    const allKzooCourseNames = _.map(MOCK_COURSES, 'kzooCourseName');
-    const uniqueKzooCourseNames = _.uniq(allKzooCourseNames);
-
-    const allKzooDisciplines = _.map(MOCK_COURSES, 'kzooDiscipline');
-    const uniqueKzooDisciplines = _.uniq(allKzooDisciplines);
-
-    const allHostInstiCourseNumbers = _.map(
-      MOCK_COURSES,
-      'hostInstiCourseNumber'
-    );
-    const uniqueHostInstiCourseNumbers = _.uniq(allHostInstiCourseNumbers);
-
-    const allHostInstiCourseNames = _.map(MOCK_COURSES, 'hostInstiCourseName');
-    const uniqueHostInstiCourseNames = _.uniq(allHostInstiCourseNames);
-
-    // combine all column options to send to front-end
-    const allColumnOptions = {
-      studyAbroadInstitutionColumnOptions: uniqueStudyAbroadInstitutions,
-      programNameColumnOptions: uniqueProgramNames,
-      yearColumnOptions: uniqueYears,
-      kzooCourseNameColumnOptions: uniqueKzooCourseNames,
-      kzooDisciplineColumnOptions: uniqueKzooDisciplines,
-      hostInstiCourseNumberColumnOptions: uniqueHostInstiCourseNumbers,
-      hostInstiCourseNamesColumnOptions: uniqueHostInstiCourseNames
-    };
-
-    // filtered courses per page
     const start = offset * size;
     const end = (offset + 1) * size;
     const sliced = _.slice(filtered, start, end);
@@ -80,7 +46,7 @@ router.get('/', (req, res) => {
     res.json({
       result: sliced,
       meta: filtered,
-      columnOptions: allColumnOptions
+      columnOptions
     });
   } catch (err) {
     console.log(err.message);
