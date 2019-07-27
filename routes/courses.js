e037c508de0b5d51c49ff2437e6b9fad38ee2dc1
Dabin Lee <40767539+BinnyDaBin@users.noreply.github.com>
2019-07-27 13:52:29 -0400

    feat($courses): add mock course data and handle get api request (#4)
    * feat($courses): add mock course data and handle get api request
    
    * chore($courses): fix suggested comments

--- routes/courses.js
+++ routes/courses.js
@@ -14,11 +14,40 @@ router.get('/', (req, res) => {
     const offset = parseInt(req.query.offset) || 0;
     const size = parseInt(req.query.size) || 10;
 
-    const filtered = _.filter(MOCK_COURSES, JSON.parse(filters));
+    const parsedFilters = JSON.parse(filters);
+
+    const filterKeys = Object.keys(parsedFilters);
+    const filtered = _.filter(MOCK_COURSES, course => {
+      for (let i = 0; i < filterKeys.length; i++) {
+        const filterKey = filterKeys[i];
+        const filterValue = parsedFilters[filterKey];
+        const courseValue = course[filterKey];
+
+        if (!filterValue.includes(courseValue)) {
+          return false;
+        }
+      }
+      return true;
+    });
+
+    const columnIds = Object.keys(MOCK_COURSES[0]);
+    const columnOptions = _.transform(
+      columnIds,
+      (allOptions, columnId) => {
+        allOptions[columnId] = _.uniq(_.map(MOCK_COURSES, columnId));
+      },
+      {}
+    );
+
     const start = offset * size;
     const end = (offset + 1) * size;
     const sliced = _.slice(filtered, start, end);
-    res.json(sliced);
+
+    res.json({
+      result: sliced,
+      meta: filtered,
+      columnOptions
+    });
   } catch (err) {
     console.log(err.message);
     res.status(400).send('Bad Request');
